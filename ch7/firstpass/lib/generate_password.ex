88ea53f683fc2510009dc1a924e5dc9f4fdca9f5
Chris Doggett <CDoggett@covermymeds.com>
2015-12-17 12:44:36 -0500

    Added other examples of how to define alphabets

--- ch7/firstpass/lib/generate_password.ex
+++ ch7/firstpass/lib/generate_password.ex
@@ -11,6 +11,9 @@ defmodule GeneratePassword do
     Macros.define_alphabet name, chars
   end
 
+  # or alphabets |> Enum.map fn {name, chars} -> Macros.define_alphabet name, chars end
+  # or Macros.define_alphabet :alpha2, "abcd1234"
+
   def generate_password(min_length, n_special, n_digits) do
     []
     |> choose_alpha(min_length - n_special - n_digits)
