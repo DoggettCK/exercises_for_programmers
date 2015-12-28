1d80fd189b49c00a27cc33ae1b6254152625c5de
Chris Doggett <CDoggett@covermymeds.com>
2015-12-28 16:04:41 -0600

    Got file upload/parsing with DropZone working

--- ch8/mp3_parser/web/router.ex
+++ ch8/mp3_parser/web/router.ex
@@ -2,7 +2,7 @@ defmodule Mp3Parser.Router do
   use Mp3Parser.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+    plug :accepts, ["html", "json"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
@@ -17,6 +17,7 @@ defmodule Mp3Parser.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    post "/upload", PageController, :upload
   end
 
   # Other scopes may use custom stacks.
