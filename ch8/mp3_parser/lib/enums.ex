af48160d0a53219855a174dee2bdf8245b39a57e
Chris Doggett <CDoggett@covermymeds.com>
2015-12-31 17:05:47 -0600

    Changed Enums to be dynamically generated

--- ch8/mp3_parser/lib/enums.ex
+++ ch8/mp3_parser/lib/enums.ex
@@ -1,29 +1,40 @@
 defmodule Enums do
   ### Picture type
-  def picture_type(0x00), do: "Other"
-  def picture_type(0x01), do: "32x32 pixels 'file icon' (PNG only)"
-  def picture_type(0x02), do: "Other file icon"
-  def picture_type(0x03), do: "Cover (front)"
-  def picture_type(0x04), do: "Cover (back)"
-  def picture_type(0x05), do: "Leaflet page"
-  def picture_type(0x06), do: "Media (e.g. label side of CD)"
-  def picture_type(0x07), do: "Lead artist/lead performer/soloist"
-  def picture_type(0x08), do: "Artist/performer"
-  def picture_type(0x09), do: "Conductor"
-  def picture_type(0x0A), do: "Band/Orchestra"
-  def picture_type(0x0B), do: "Composer"
-  def picture_type(0x0C), do: "Lyricist/text writer"
-  def picture_type(0x0D), do: "Recording Location"
-  def picture_type(0x0E), do: "During recording"
-  def picture_type(0x0F), do: "During performance"
-  def picture_type(0x10), do: "Movie/video screen capture"
-  def picture_type(0x11), do: "A bright coloured fish"
-  def picture_type(0x12), do: "Illustration"
-  def picture_type(0x13), do: "Band/artist logotype"
-  def picture_type(0x14), do: "Publisher/Studio logotype"
+  for {val, desc} <- %{
+    0x00 => "Other",
+    0x01 => "32x32 pixels 'file icon' (PNG only)",
+    0x02 => "Other file icon",
+    0x03 => "Cover (front)",
+    0x04 => "Cover (back)",
+    0x05 => "Leaflet page",
+    0x06 => "Media (e.g. label side of CD)",
+    0x07 => "Lead artist/lead performer/soloist",
+    0x08 => "Artist/performer",
+    0x09 => "Conductor",
+    0x0A => "Band/Orchestra",
+    0x0B => "Composer",
+    0x0C => "Lyricist/text writer",
+    0x0D => "Recording Location",
+    0x0E => "During recording",
+    0x0F => "During performance",
+    0x10 => "Movie/video screen capture",
+    0x11 => "A bright coloured fish",
+    0x12 => "Illustration",
+    0x13 => "Band/artist logotype",
+    0x14 => "Publisher/Studio logotype",
+  } do
+    def picture_type(unquote(val)), do: unquote(desc)
+  end
+
   def picture_type(_), do: "Unknown picture type"
 
   ### Encoding
-  def encoding(0), do: "ISO-8859-1"
-  def encoding(1), do: "UTF-8"
+  for {val, desc} <- %{
+    0x00 => "ISO-8859-1",
+    0x01 => "UTF-8",
+  } do
+    def encoding(unquote(val)), do: unquote(desc)
+  end
+
+  def encoding(_), do: "Unknown encoding"
 end
