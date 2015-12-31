60a6efeef2887b3e5c16a43d457378227bc6a000
Chris Doggett <CDoggett@covermymeds.com>
2015-12-31 17:09:14 -0600

    Rewrote v2.2 parser with macros

--- ch8/mp3_parser/lib/id3_v2_2_parser.ex
+++ ch8/mp3_parser/lib/id3_v2_2_parser.ex
@@ -1,9 +1,22 @@
 defmodule ID3_v2_2_Parser do
-  def parse_frame(data_dict, << "TSS", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
+  use Macros.ID3_v2_2
 
-    parse_frame(data_dict |> Dict.put("encoding_settings", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
+  text_frame "TAL", "album"
+  text_frame "TCM", "composer"
+  text_frame "TDA", "date"
+  text_frame "TIM", "time"
+  text_frame "TP1", "lead_performer"
+  text_frame "TSS", "encoding_settings"
+  text_frame "TT2", "title"
+  text_frame "TYE", "year"
+  
+  # iTunes custom tags
+  text_frame "TSA", "album_sort"
+  text_frame "TSC", "composer_sort"
+  text_frame "TSP", "artist_sort"
+  text_frame "TST", "title_sort"
+
+  integer_frame "TBP", "beats_per_minute"
 
   def parse_frame(data_dict, << "COM", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
     << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
@@ -13,30 +26,6 @@ defmodule ID3_v2_2_Parser do
     parse_frame(data_dict |> Dict.put("comments", comments), remaining_frame_data)
   end
 
-  def parse_frame(data_dict, << "TDA", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("date", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-
-  def parse_frame(data_dict, << "TIM", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("time", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TYE", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("year", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TBP", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("beats_per_minute", frame_data |> StringUtils.decode_string |> String.to_integer), remaining_frame_data)
-  end
-  
   def parse_frame(data_dict, << "PIC", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
     << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
 
@@ -44,59 +33,6 @@ defmodule ID3_v2_2_Parser do
 
     parse_frame(data_dict |> Dict.put("pictures", pictures), remaining_frame_data)
   end
-  
-  def parse_frame(data_dict, << "TT2", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("title", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TP1", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("lead_performer", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TCM", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("composer", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TAL", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    parse_frame(data_dict |> Dict.put("album", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TST", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    # iTunes custom tag
-    parse_frame(data_dict |> Dict.put("title_sort", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TSA", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    # iTunes custom tag
-    parse_frame(data_dict |> Dict.put("album_sort", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-  
-  def parse_frame(data_dict, << "TSP", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    # iTunes custom tag
-    parse_frame(data_dict |> Dict.put("artist_sort", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-
-  def parse_frame(data_dict, << "TSC", size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
-    << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
-
-    # iTunes custom tag
-    parse_frame(data_dict |> Dict.put("composer_sort", frame_data |> StringUtils.decode_string), remaining_frame_data)
-  end
-
 
   def parse_frame(data_dict, << 0, 0, 0, _::binary >>) do
     # Null tag means there's nothing left
@@ -114,7 +50,7 @@ defmodule ID3_v2_2_Parser do
   end
 
   ### APIC frame parsing
-  defp parse_picture_frame(<<0, image_format::binary-size(3), picture_type::unsigned-integer-size(8), remaining_data::binary>> = frame_data) do
+  defp parse_picture_frame(<<0, image_format::binary-size(3), picture_type::unsigned-integer-size(8), remaining_data::binary>>) do
     [description, picture_data] = remaining_data |> String.split(<<0>>, parts: 2)
 
     %{}
@@ -126,7 +62,7 @@ defmodule ID3_v2_2_Parser do
   end
 
   ### Comment frame parsing
-  defp parse_comment_frame(<< 0, language::binary-size(3), comment_data::binary >> = frame_data) do
+  defp parse_comment_frame(<< 0, language::binary-size(3), comment_data::binary >>) do
     [short_description, actual_text | _] = comment_data |> String.split(<<0>>)
 
     %{}
@@ -136,7 +72,7 @@ defmodule ID3_v2_2_Parser do
     |> Dict.put("text", actual_text)
   end
 
-  defp parse_comment_frame(<< 1, language::binary-size(3), comment_data::binary >> = frame_data) do
+  defp parse_comment_frame(<< 1, language::binary-size(3), comment_data::binary >>) do
     # Reattach the encoding so the decoder can parse them correctly
     [short_description, actual_text | _] = comment_data 
                                             |> String.split(<<0, 0>>)
---
+++ ch8/mp3_parser/lib/macros.ex
@@ -0,0 +1,32 @@
+defmodule Macros do
+  defmodule ID3_v2_2 do
+    defmacro __using__(_options) do
+      quote do
+        import unquote(__MODULE__)
+      end
+    end
+
+    defp simple_frame(tag, json_name, transform) do
+      final_ast = quote do
+        def parse_frame(data_dict, << unquote(tag), size::unsigned-integer-size(24), remaining_frame_data::binary >>) do
+          << frame_data :: binary-size(size), remaining_frame_data :: binary >> = remaining_frame_data
+
+          value = unquote(transform)
+
+          parse_frame(data_dict |> Dict.put(unquote(json_name), value), remaining_frame_data)
+        end
+      end
+
+      IO.puts Macro.to_string(final_ast)
+      final_ast
+    end    
+
+    defmacro text_frame(tag, json_name) do
+      simple_frame(tag, json_name, quote(do: frame_data |> StringUtils.decode_string))
+    end
+
+    defmacro integer_frame(tag, json_name) do
+      simple_frame(tag, json_name, quote(do: frame_data |> StringUtils.decode_string |> String.to_integer))
+    end    
+  end
+end
