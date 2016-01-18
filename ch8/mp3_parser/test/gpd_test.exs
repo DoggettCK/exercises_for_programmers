df27e2a4d18d8e6e8e4fffb46095cc069706d5aa
Chris Doggett <CDoggett@covermymeds.com>
2016-01-18 12:36:54 -0600

    Added test for MS filetime conversion

--- ch8/mp3_parser/test/gpd_test.exs
+++ ch8/mp3_parser/test/gpd_test.exs
@@ -1,7 +1,8 @@
 defmodule GpdTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "MS filetime stamp converter works properly" do
+    assert 1452874705000 == TimeUtils.filetime_to_datetime(130973483050000000)
+    assert 130973483050000000 == TimeUtils.datetime_to_filetime(1452874705000)
   end
 end
